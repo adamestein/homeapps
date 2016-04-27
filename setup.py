e62bb642a183d8256250e016972022528aa721d8
Adam Stein <adam@csh.rit.edu>
2016-04-27 16:34:39 -0400

    Added post-deploy script with some debugging stuff to figure out what needs to be set

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(name='HomeApps',
-      version='1.0',
+      version='3.0',
       description='Apps for things in the home',
       author='Adam Stein',
       author_email='adam@csh.rit.edu',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.6.11'],
+      url='http://www.python.org/sigs/distutils-sig/'
      )
