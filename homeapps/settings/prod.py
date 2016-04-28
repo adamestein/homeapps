d1e62f3f8dcc4e84d2c328743dd8c380308275f8
Adam Stein <adam@csh.rit.edu>
2016-04-28 16:47:40 -0400

    Added installing django-extensions for dev requirements
    Updated settings to automatically determine if on production or dev server (so that I don't need
    to specify settings when running manage.py on command line)

--- homeapps/settings/prod.py
+++ homeapps/settings/prod.py
@@ -1,1 +1,12 @@
 from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['OPENSHIFT_APP_NAME'],
+        'USER': os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+        'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+        'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+        'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
+    }
+}
