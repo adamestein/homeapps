34f2018fe7dedac5bbd3ac6b5a1e92155560f046
Adam Stein <adam@zookeeper.steinhome.net>
2016-04-30 21:20:39 -0400

    Added list page for smoke detectors

--- apps/library/views/generic/__init__.py
+++ apps/library/views/generic/__init__.py
@@ -1,1 +1,2 @@
 from .base import NavigationTemplateView
+from .list import NavigationListView
--- apps/smoke_detectors/urls.py
+++ apps/smoke_detectors/urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls import patterns, url
 
-from library.views.generic import NavigationTemplateView
+from .models import SmokeDetector
+
+from library.views.generic import NavigationListView, NavigationTemplateView
 
 urlpatterns = patterns(
     'smoke_detectors',
 
-    # Top page
-    url('^$', NavigationTemplateView.as_view(template_name='smoke_detectors/home.html'), name='smoke_detectors')
+    url('^$', NavigationTemplateView.as_view(template_name='smoke_detectors/home.html'), name='smoke_detectors'),
+
+    url(
+        '^list/$',
+        NavigationListView.as_view(model=SmokeDetector, template_name='smoke_detectors/list.html'),
+        name='list_smoke_detectors'
+    )
 )
