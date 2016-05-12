dac038b1262cc5ae1c3c8c108e800ab700461f27
Adam Stein <adam@csh.rit.edu>
2016-05-12 13:54:33 -0400

    Added custom 500.html page. Added logging handler to save Django errors to help debug 'Server 500' errors.

--- homeapps/settings/prod.py
+++ homeapps/settings/prod.py
@@ -10,3 +10,26 @@ DATABASES = {
         'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']
     }
 }
+
+LOG_DIR = os.environ.get('OPENSHIFT_LOG_DIR')
+
+# Logging handlers
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(LOG_DIR, 'django.log'),
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
