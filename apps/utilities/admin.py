aa7f1573f7bfb06bf738bbccad212ee749f5f8a4
Adam Stein <adam@csh.rit.edu>
2026-06-27 09:49:20 -0400

    Updated utilities django admin to be able to filter statements by year

--- apps/utilities/admin.py
+++ apps/utilities/admin.py
@@ -3,5 +3,33 @@ from django.contrib import admin
 from .models import ElectricGasStatement, WaterStatement
 
 
-admin.site.register(ElectricGasStatement)
-admin.site.register(WaterStatement)
+class YearFilter(admin.SimpleListFilter):
+    title = 'year'
+    parameter_name = 'year'
+
+    def lookups(self, request, model_admin):
+        years = model_admin.model.objects.dates(self.field_name, 'year')
+        return [(year.year, year.year) for year in years]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(**{f'{self.field_name}__year': self.value()})
+        return queryset
+
+
+class ElectricGasYearFilter(YearFilter):
+    field_name = 'statement_date'
+
+
+class WaterYearFilter(YearFilter):
+    field_name = 'from_date'
+
+
+@admin.register(ElectricGasStatement)
+class ElectricGasStatementAdmin(admin.ModelAdmin):
+    list_filter = (ElectricGasYearFilter,)
+
+
+@admin.register(WaterStatement)
+class WaterStatementAdmin(admin.ModelAdmin):
+    list_filter = (WaterYearFilter,)
