3d30718a68c463f2ba938535f48c5e6adc465e01
Adam Stein <adam@csh.rit.edu>
2026-06-27 10:30:54 -0400

    Updated utility view pages so no data is displayed until everything is properly formatted

--- apps/utilities/tests/test_select.py
+++ apps/utilities/tests/test_select.py
@@ -1,8 +1,11 @@
+from datetime import date
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
 from .forms import SelectForm
+from .models import ElectricGasStatement, WaterStatement
 
 
 class SelectFormTests(TestCase):
@@ -59,3 +62,45 @@ class StatementAddViewTests(TestCase):
             '<input type="number" name="amount" step="0.01" required autofocus id="id_amount">',
             html=True
         )
+
+
+class DataListViewTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test-user', password='password')
+        self.client.login(username='test-user', password='password')
+
+        ElectricGasStatement.objects.create(
+            user=self.user,
+            amount='123.45',
+            electric_used=1000,
+            gas_used=35,
+            statement_date=date(2026, 1, 15)
+        )
+        WaterStatement.objects.create(
+            user=self.user,
+            amount='67.89',
+            from_date=date(2026, 1, 1),
+            to_date=date(2026, 1, 31),
+            water_used=12
+        )
+
+    def test_electric_gas_data_is_not_displayed_before_year_is_selected(self):
+        response = self.client.get(reverse('utilities:view_data', args=['electric_gas']))
+
+        self.assertNotContains(response, '<div id="tabs">')
+        self.assertNotContains(response, 'Electric<br')
+        self.assertNotContains(response, '$123.45')
+
+    def test_water_data_is_not_displayed_before_year_is_selected(self):
+        response = self.client.get(reverse('utilities:view_data', args=['water']))
+
+        self.assertNotContains(response, '<div id="tabs">')
+        self.assertNotContains(response, 'Water<br')
+        self.assertNotContains(response, '$67.89')
+
+    def test_data_tabs_are_hidden_until_initialized(self):
+        response = self.client.get(reverse('utilities:view_data', args=['water']), {'year': 2026})
+
+        self.assertContains(response, '<div id="tabs">')
+        self.assertContains(response, '#tabs {\n            margin-top: 2em;\n            visibility: hidden;')
+        self.assertContains(response, 'tabs.css("visibility", "visible");')
